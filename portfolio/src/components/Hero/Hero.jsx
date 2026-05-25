405a493ee4fec74e2f523e9198c049f614304f30
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-05-25 14:32:09 +0200

    feat: create Hero component

---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import "./Hero.css";
+
+function Hero() {
+    
+    return (
+        <section className="hero">
+
+            <h1 className="hero-title">
+                Ramzi Chejjar
+            </h1>
+
+            <p className="hero-subtitle">
+                Sofrware Engineering Student
+            </p>
+        </section>
+    );
+
+}
+
+export default Hero;
