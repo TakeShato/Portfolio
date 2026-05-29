6129470c32071cd2fe4d29f87879405b0751fe42
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-05-29 02:11:38 +0200

    feat: started on  the contact page

---
+++ portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import "./Contact.css"
+
+function Contact() {
+
+    return (
+
+        <section id="contact" className="contact">
+
+            <h2>Contact</h2>
+
+            <p>
+                Interested in working together or discussing a project?
+            </p>
+
+            <div className="contact-links">
+
+                <a href="#">
+                    LinkedIn
+                </a>
+
+                <a href="#">
+                    GitHub
+                </a>
+
+                <a href="#">
+                    Email
+                </a>
+
+            </div>
+
+        </section>
+
+    );
+
+}
+
+export default Contact;
