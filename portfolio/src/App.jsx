d90bc184c48b0ef03c12110bc5a164d6eb0d1ab7
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-05-29 02:11:19 +0200

    feat: added animatedroutes and routes to all the projects

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,74 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 
-function App() {
+import GameWebshop from "./pages/GameWebshop";
+import CodeExchange from "./pages/codeExchange";
+import Elections from "./pages/Elections";
+
+import { AnimatePresence } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import Contact from "./components/Contact/Contact";
+
+function Home() {
   
   return (
     <>
     <Hero />
     <About />
     <Projects />
+    <Contact />
     </>
+  );
+
+}
+
+function App() {
+
+  return (
+
+    <BrowserRouter>
+
+      <AnimatedRoutes />
+
+    </BrowserRouter>
+
+  );
+
+}
+
+function AnimatedRoutes() {
+
+  const location = useLocation();
+
+  return (
+
+    <AnimatePresence mode="wait">
+
+      <Routes location={location} key={location.pathname}>
+
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/gamewebshop"
+          element={<GameWebshop />}
+        />
+
+        <Route
+          path="/elections"
+          element={<Elections />}
+        />
+
+        <Route
+          path="/codeexchange"
+          element={<CodeExchange />}
+        />
+
+      </Routes>
+
+    </AnimatePresence>
+
   );
 
 }
