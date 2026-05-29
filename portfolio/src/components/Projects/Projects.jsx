d60719433b775ee557cacfd6f2ea53903d7765c8
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-05-29 02:10:28 +0200

    feat: made it so you can click on the widget and get sent to the detailed description page of the project

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import "./Projects.css";
+import { Link } from "react-router-dom";
 
 function Projects() {
 
@@ -12,17 +13,17 @@ function Projects() {
                     <h3>AI tracker</h3>
                 </div>
 
-                <div className="projects-panel panel-2">
+                <Link to="/gamewebshop" className="projects-panel panel-2">
                     <h3>Game Webshop</h3>
-                </div>
+                </Link>
 
-                <div className="projects-panel panel-3">
+                <Link to="/codeexchange" className="projects-panel panel-3">
                     <h3>Code Exchange</h3>
-                </div>
+                </Link>
 
-                <div className="projects-panel panel-4">
+                <Link to="/elections" className="projects-panel panel-4">
                     <h3>Elections</h3>
-                </div>
+                </Link>
 
             </div>
 
